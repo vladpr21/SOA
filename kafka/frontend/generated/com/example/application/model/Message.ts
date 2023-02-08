9892deae89645d600d510e5477b3f3abdb5665ee
vladprundus <prundus_vlad@yahoo.com>
2023-02-08 16:56:36 +0200

    added kafka and client

---
+++ kafka/frontend/generated/com/example/application/model/Message.ts
@@ -0,0 +1,11 @@
+
+/**
+ * This module is generated from com.example.application.model.Message.
+ * All changes to this file are overridden. Please consider to make changes in the corresponding Java file if necessary.
+ * @see {@link file:///Users/vladprundus/Documents/soa/project/Service-Oriented-Architecture/kafka/src/main/java/com/example/application/model/Message.java}
+ */
+export default interface Message {
+  text: string;
+  time?: string;
+  userName: string;
+}
